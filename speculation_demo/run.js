05af7fa2df751a47eb1f61f403681e6efa14ae4c
Sabri Meeran <81985168+sabrimeeran@users.noreply.github.com>
2021-04-09 00:26:55 +0100

    Update run.js

--- speculation_demo/run.js
+++ speculation_demo/run.js
@@ -1,14 +1,14 @@
 new Chart(document.getElementById("line-chart"), {
     type: 'line',
     data: {
-        // labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+        labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
         datasets: [{
-            data: FED_BTC_DATA,
-            label: "Bitcoin",
+            data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+            label: "Africa",
             borderColor: "#3e95cd",
             fill: false
         }
-        ]
+        ] // how do we just show, e.g. 20 values, of interest using pure JS? // Dates on x-axis?
     },
     options: {
         title: {
