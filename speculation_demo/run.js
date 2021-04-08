45a7f3bf66cb49a9525210b5a75e9c3f3e1e4e72
Sabri Meeran <81985168+sabrimeeran@users.noreply.github.com>
2021-04-08 22:33:17 +0100

    Update run.js

--- speculation_demo/run.js
+++ speculation_demo/run.js
@@ -1,17 +1,19 @@
-const CHART = document.getElementById('lineChart');
+var ctx = document.getElementById('lineChart');
 
+const labels = Utils.months({ count: 7 });
 const data = {
+    labels: labels,
     datasets: [{
         label: 'My First Dataset',
         data: [65, 59, 80, 81, 56, 55, 40],
         fill: false,
-        borderColor: 'rgb(75, 192, 192)'
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
     }]
 };
 
-let lineChart = new Chart(CHART, {
+var myLineChart = new Chart(ctx, {
     type: 'line',
-    data: data
+    data: data,
+    options: options
 });
-
-
