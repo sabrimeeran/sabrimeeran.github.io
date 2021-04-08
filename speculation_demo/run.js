cd12f8d0861d07b0919efc3adf41cd5cbb250643
Sabri Meeran <81985168+sabrimeeran@users.noreply.github.com>
2021-04-08 17:02:17 +0100

    Trial

--- speculation_demo/run.js
+++ speculation_demo/run.js
@@ -1,27 +1,5 @@
 var ctx = document.getElementById('priceChart');
 
 // test to see if works!
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        datasets: [{
-            label: '# of Votes',
-            data: FED_BTC_DATA,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+
 
