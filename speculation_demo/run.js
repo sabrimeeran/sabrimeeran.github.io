3220261876da2df24fde963a20ced5163dbc45d9
Sabri Meeran <81985168+sabrimeeran@users.noreply.github.com>
2021-04-08 22:30:02 +0100

    Trial 2

--- speculation_demo/run.js
+++ speculation_demo/run.js
@@ -1,5 +1,17 @@
-var ctx = document.getElementById('priceChart');
+const CHART = document.getElementById('lineChart');
 
-// test to see if works!
+const data = {
+    datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)'
+    }]
+};
+
+let lineChart = new Chart(CHART, {
+    type: 'line',
+    data: data
+});
 
 
