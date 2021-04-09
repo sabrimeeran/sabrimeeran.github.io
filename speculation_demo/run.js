f9df06dc76457b65a9dd3697d1d8c9af2a2bb801
Sabri Meeran <81985168+sabrimeeran@users.noreply.github.com>
2021-04-10 00:33:57 +0100

    Update run.js

--- speculation_demo/run.js
+++ speculation_demo/run.js
@@ -1,10 +1,47 @@
+
+var dates = []; // on x-axis
+var prices = []; // on y-axis
+
+datesAtIntervals(1, 2021, 1, 2022, 1);
+
+function datesAtIntervals(monthInterval, startYear, startMonth, finalYear, finalMonth) {
+    var currentYear = startYear;
+    var currentMonth = startMonth;
+    var monthFormat = '';
+
+    if (startYear > finalYear) {
+        return;
+    } else if (startMonth > finalMonth) {
+        return;
+    }
+    for (i = 0; i < 20; i++) {
+
+        monthFormat = (currentMonth + 1).toString()
+        if (monthFormat.length == 1) {
+            monthFormat = '0' + monthFormat;
+        }
+        var dateString = monthFormat + '/01/' + currentYear
+
+        if (currentMonth == 11) { // months are indexed starting 0
+            currentMonth = 0;
+            currentYear += 1;
+        } else {
+            currentMonth += 1;
+        }
+
+    }
+
+
+
+}
+
 new Chart(document.getElementById("line-chart"), {
     type: 'line',
     data: {
-        labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+        labels: [FED_BTC_DATA[0].DATE, FED_BTC_DATA[1].DATE],
         datasets: [{
-            data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
-            label: "Africa",
+            data: [FED_BTC_DATA[0].CBBTCUSD, FED_BTC_DATA[1].CBBTCUSD],
+            label: "Price",
             borderColor: "#3e95cd",
             fill: false
         }
